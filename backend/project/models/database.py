d8f81d3123da1a3b3c16a94b4c9dde0215f04225
kairat <112894056+kairat-genius@users.noreply.github.com>
2024-03-10 10:58:11 +0300

    db

--- backend/project/models/database.py
+++ backend/project/models/database.py
@@ -3,11 +3,9 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from .models import metadata
 
 #Базы данных
-DATABASE_URL = ("sqlite:///../database.db")
+DATABASE_URL = ("sqlite:///project/database.db")
 engine = create_engine(
     DATABASE_URL, echo=True, connect_args={"check_same_thread": False}
 )
 
 session_local = sessionmaker(bind=engine)
-
-metadata.create_all(engine)
